13e28b95a967ea0d2960bfabc655cb8472b98bef
MattScho <42156587+MattScho@users.noreply.github.com>
2018-11-08 07:56:33 -0500

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,20 @@
+
+public class Item {
+
+	private String name;
+	private int numberOf;
+	
+	public Item(String name, int numberOf)
+	{
+		this.name = name;
+		this.numberOf = numberOf;
+	}
+	
+	public void addToNumberOf(int amountToAdd)
+	{
+		numberOf += amountToAdd;
+	}
+	
+	
+	
+}
